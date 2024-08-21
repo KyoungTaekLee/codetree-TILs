5a90ff4cfcd867914081639368e87f1068fe2249
KyoungTaekLee <124133848+KyoungTaekLee@users.noreply.github.com>
2024-08-21 11:11:47 +0900

    Title: 달에서 무게 구하기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240821/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int chuWeight = 13;
+        double moonWeight = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f",chuWeight,moonWeight,chuWeight*moonWeight);
+    }
+}
