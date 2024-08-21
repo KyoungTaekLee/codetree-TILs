8bd4929a2806f306888f9d61d28f7a4eda4a7ac9
KyoungTaekLee <124133848+KyoungTaekLee@users.noreply.github.com>
2024-08-21 15:47:29 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

--- 240821/데이터 교환/exchange-data.java
+++ 240821/데이터 교환/exchange-data.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int a = 5, b = 6, c = 7, temp1, temp2;
+        int a = 5, b = 6, c = 7, temp;
 
-        temp1 = c;
-        temp2 = b;
-        b = a;
-        c = temp2;
-        a = temp1;
-
+        temp = a;
+        a = c;
+        c = b;
+        b = temp;
+        
         System.out.print(a+"\n"+b+"\n"+c);
     }
 }
